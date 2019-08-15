4327600b1d6b0bd0dbc3dae56433499ad118ee8d
Kirill Osenkov <github@osenkov.com>
2019-08-15 16:41:15 -0700

    Add IVT and update idebuild.cmd.

--- idebuild.cmd
+++ idebuild.cmd
@@ -1,4 +1,1 @@
-restore.cmd
-REM build.cmd -release -official -sign -binaryLog
-
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /nr:false /clp:v=m /p:Configuration=Release /m /bl:C:\roslyn\Binaries\Logs\Roslyn.binlog /p:DeployExtension=false /p:SkipApplyOptimizations=true /p:BuildNumber=20180315.5 /p:OfficialBuild=true /p:RoslynDebugType=embedded Roslyn.sln
+build -restore -binaryLog -configuration Release /p:OfficialBuild=true /p:DebugType=embedded
