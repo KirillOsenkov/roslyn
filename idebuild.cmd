31b68adce04a3049f23ec0787fd0b7508f761e30
Kirill Osenkov <github@osenkov.com>
2020-06-07 15:56:36 -0700

    Update idebuild.cmd

--- idebuild.cmd
+++ idebuild.cmd
@@ -1,3 +1,3 @@
-build -restore -binaryLog -configuration Release /p:OfficialBuild=true /p:DebugType=embedded /p:VisualStudioDropName=Products/drop /p:RepositoryName=VSRepo
-msbuild /bl copy.proj
-nuget pack roslyn.nuspec
+call build -restore -binaryLog -configuration Release /p:OfficialBuild=true /p:DebugType=embedded /p:VisualStudioDropName=Products/drop /p:RepositoryName=VSRepo
+call msbuild /bl copy.proj
+call nuget pack roslyn.nuspec -Version 1.0.11
