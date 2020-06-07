c6bf5f277fc51384a1011b2fa2ce65ede7ad9e4e
Kirill Osenkov <github@osenkov.com>
2020-06-06 17:44:56 -0700

    Include Microsoft.CodeAnalysis.CSharp.EditorFeatures.Wpf and InteractiveHost* in the NuGet

--- idebuild.cmd
+++ idebuild.cmd
@@ -1,1 +1,3 @@
-build -restore -binaryLog -configuration Release /p:OfficialBuild=true /p:DebugType=embedded
+build -restore -binaryLog -configuration Release /p:OfficialBuild=true /p:DebugType=embedded /p:VisualStudioDropName=Products/drop /p:RepositoryName=VSRepo
+msbuild /bl copy.proj
+nuget pack roslyn.nuspec
