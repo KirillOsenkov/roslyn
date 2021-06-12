e7801d7b26c464e6bc1db10468168214f88fd9fa
Kirill Osenkov <github@osenkov.com>
2021-06-11 19:45:36 -0700

    IDE changes.

--- src/EditorFeatures/Core.Wpf/Interactive/InteractiveEvaluator.cs
+++ src/EditorFeatures/Core.Wpf/Interactive/InteractiveEvaluator.cs
@@ -189,9 +189,11 @@ namespace Microsoft.CodeAnalysis.Editor.Interactive
 
             var resetOptions = ResetOptions;
             _session.Host.SetOutputs(window.OutputWriter, window.ErrorOutputWriter);
-            var isSuccessful = await _session.ResetAsync(_session.GetHostOptions(initialize: true, resetOptions.Platform)).ConfigureAwait(false);
+            var isSuccessful = await _session.ResetAsync(HostOptions ?? _session.GetHostOptions(initialize: true, resetOptions.Platform)).ConfigureAwait(false);
             return new ExecutionResult(isSuccessful);
         }
+
+        public InteractiveHostOptions? HostOptions { get; set; }
 
         /// <summary>
         /// Invoked by the reset toolbar button.
@@ -208,7 +210,7 @@ namespace Microsoft.CodeAnalysis.Editor.Interactive
             window.WriteLine(EditorFeaturesWpfResources.Resetting_execution_engine);
             window.FlushOutput();
 
-            var options = _session.GetHostOptions(initialize, resetOptions.Platform);
+            var options = HostOptions ?? _session.GetHostOptions(initialize, resetOptions.Platform);
             OnBeforeReset?.Invoke(options.Platform);
             var isSuccessful = await _session.ResetAsync(options).ConfigureAwait(false);
             return new ExecutionResult(isSuccessful);
@@ -266,10 +268,7 @@ namespace Microsoft.CodeAnalysis.Editor.Interactive
 
         public string GetPrompt()
         {
-            var buffer = GetInteractiveWindow().CurrentLanguageBuffer;
-            return buffer != null && buffer.CurrentSnapshot.LineCount > 1
-                ? ". "
-                : "> ";
+            return "";
         }
 
         public Task SetPathsAsync(ImmutableArray<string> referenceSearchPaths, ImmutableArray<string> sourceSearchPaths, string workingDirectory)
