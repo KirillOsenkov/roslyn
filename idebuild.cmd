0acde32dc28d0aad6bbce14cf1ed5e4d08a6c102
Kirill Osenkov <github@osenkov.com>
2024-04-25 16:41:26 -0700

    Update for https://github.com/dotnet/roslyn/pull/73237

--- idebuild.cmd
+++ idebuild.cmd
@@ -11,4 +11,4 @@ if %errorlevel% NEQ 0 (
 	exit /b %errorlevel%
 )
 
-call nuget pack microsoft.ide.internal.roslyn.nuspec -Version 1.0.8
+call nuget pack microsoft.ide.internal.roslyn.nuspec -Version 1.0.10
